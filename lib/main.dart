ad8288b2ecb3e56ce9c6a7babb96534b54776443
lidaev <lidiya.edilgiriyeva@gmail.com>
2018-10-14 12:39:37 +0300

    Change MockData

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,47 @@ import 'package:one_two_quest/list_promo.dart';
 
 void main() => runApp(new MyApp());
 
+Map data = {
+  "places": [
+    {
+      "title": "Окинава",
+      "coordinates": {
+        "lat": "55.788410",
+        "lon": "49.121562"
+      },
+      "type": "Ресторан",
+      "Rating": "4.5"
+    },
+    {
+      "title": "Нефть",
+      "coordinates": {
+        "lat": "55.787861",
+        "lon": "49.120184"
+      },
+      "type": "Кофейня",
+      "Rating": "4.6"
+    },
+    {
+      "title": "Музей советского быта",
+      "coordinates": {
+        "lat": "55.786851",
+        "lon": "49.119465"
+      },
+      "type": "Музей",
+      "Rating": "5.0"
+    },
+    {
+      "title": "Шоколадница",
+      "coordinates": {
+        "lat": "55.785982",
+        "lon": "49.119904"
+      },
+      "type": "Кофейня",
+      "Rating": "4.1"
+    }
+  ]
+};
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -18,7 +59,7 @@ class MyApp extends StatelessWidget {
               appBar: AppBar(
                 bottom: TabBar(
                   tabs: [
-                    Tab(text: "КАРТА"),
+                    Tab(text: data["places"][0]["title"]),
                     Tab(text: "МОИ КУПОНЫ"),
                   ],
                 ),
